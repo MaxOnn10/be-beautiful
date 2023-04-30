52d54802f7d343d565facc2e681079151b0b4ffc
MaxOnn10 <maks.melenevskiy@gmail.com>
2023-04-30 20:17:24 +0300

    new burger menu + header navigation is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
-const burgerMenu = document.querySelector('.burger-menu');
+// const burgerMenu = document.querySelector('.burger-menu');
 var updateButtons = document.getElementsByClassName('awards__item-button');
 var cancelButtons = document.getElementsByClassName('awards__item-button--close');
 var favDialogs = document.getElementsByClassName('awards__item-dialog');
 
-burgerMenu.addEventListener('click', function () {
-  this.classList.toggle('active');
-});
+// burgerMenu.addEventListener('click', function () {
+//   this.classList.toggle('active');
+// });
 
 for (let i = 0; i < updateButtons.length; i++) {
   updateButtons[i].addEventListener("click", function() {
